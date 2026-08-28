5ecde96722691fbb3de53b6d73634fa234421ef1
Sa4eNecHinS <sanek.evteev2002@gmail.com>
2026-08-28 14:44:36 +0300

    update hyprlock and clean things

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,9 +19,31 @@ return {
         pyright = {
           enabled = false,
         },
-        pylsp = {},
-        marksman = false,
+        pylsp = {
+          plugins = {
+            -- formatter options
+            black = { enabled = true },
+            autopep8 = { enabled = false },
+            yapf = { enabled = false },
+            -- linter options
+            pylint = { enabled = true, executable = "pylint" },
+            ruff = { enabled = false },
+            pyflakes = { enabled = false },
+            pycodestyle = { enabled = false },
+            -- type checker
+            pylsp_mypy = {
+              enabled = true,
+              report_progress = true,
+              live_mode = false,
+            },
+            -- auto-completion options
+            jedi_completion = { fuzzy = true },
+            -- import sorting
+            isort = { enabled = true },
+          },
+        },
       },
+      marksman = false,
     },
   },
 }
